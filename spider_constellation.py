2317cce58194b6863094f0d7594a8eeb9015a9d0
Jerome-bouvier <moeb.bzhash@gmail.com>
2018-04-10 11:09:31 +0200

    hop

---
+++ spider_constellation.py
@@ -0,0 +1,12 @@
+import scrapy
+
+
+class BlogSpider(scrapy.Spider):
+    name = 'blogspider'
+    start_urls = ['http://cstrecords.com/artists/']
+
+    def parse(self, response):
+        for artist in response.css('div#activeartists h5 a'):
+            yield {'artist': link.css('a ::text').extract_first()}
+
+EOF
